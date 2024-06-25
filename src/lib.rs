3c0feb5911b97330fccc2e10849e0d4d66b9b964
drew458 <43808240+drew458@users.noreply.github.com>
2024-06-25 13:04:21 +0200

    fixed struct impl

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ enum LockType {
 
 impl DistributedLock {
 
-    pub fn new(key: i128, lock_type: LockType, is_shared: bool) {
+    pub fn new(key: i128, lock_type: LockType, is_shared: bool) -> DistributedLock {
 
         DistributedLock {
             key,
@@ -25,10 +25,22 @@ impl DistributedLock {
     }
 
     pub fn lock_type(&self) -> &LockType {
-        &self.loc
+        &self.lock_type
     }
 
     pub fn is_shared(&self) -> &bool {
-        &self.key
+        &self.is_shared
+    }
+
+    pub fn set_key(&mut self, key: i128) {
+        self.key = key
+    }
+
+    pub fn set_lock_type(&mut self, lock_type: LockType) {
+        self.lock_type = lock_type
+    }
+
+    pub fn set_is_shared(&mut self, is_shared: bool) {
+        self.is_shared = is_shared
     }
 }
