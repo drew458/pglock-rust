da20acf75d7bcbb70f5572a42f802e117b37c147
drew458 <43808240+drew458@users.noreply.github.com>
2024-06-26 15:30:12 +0200

    changed ownership of objects

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,33 @@
 use sqlx::PgPool;
 
-#[derive(Debug, Clone, Copy)]
-pub struct DistributedLock {
+struct DistributedLock<'a> {
+    pool: &'a PgPool,
     key: i64,
     lock_type: LockType,
     is_shared: bool
 }
 
-#[derive(Debug, Clone, Copy)]
 pub enum LockType {
     SessionLock,
     TransactionLock
 }
 
-impl DistributedLock {
+impl <'a> DistributedLock<'a> {
 
-    pub fn new(key: i64) -> DistributedLock {
+    pub fn new(pool: &PgPool, key: i64) -> DistributedLock {
 
         DistributedLock {
+            pool,
             key,
             lock_type: LockType::SessionLock,
             is_shared: false
         }
     }
 
-    pub fn new_with_attributes(key: i64, lock_type: LockType, is_shared: bool) -> DistributedLock {
+    pub fn new_with_attributes(pool: &PgPool, key: i64, lock_type: LockType, is_shared: bool) -> DistributedLock {
 
         DistributedLock {
+            pool,
             key,
             lock_type,
             is_shared
@@ -37,8 +38,8 @@ impl DistributedLock {
         self.key
     }
 
-    pub fn lock_type(&self) -> LockType {
-        self.lock_type
+    pub fn lock_type(&self) -> &LockType {
+        &self.lock_type
     }
 
     pub fn is_shared(&self) -> bool {
@@ -56,31 +57,30 @@ impl DistributedLock {
     pub fn set_is_shared(&mut self, is_shared: bool) {
         self.is_shared = is_shared
     }
-}
 
-pub async fn lock(pool: &PgPool, lock: &DistributedLock) -> Result<(), sqlx::Error> {
+    pub async fn lock(&self) -> Result<(), sqlx::Error> {
+        sqlx::query(&build_query(self.is_shared(), true))
+        .bind(self.key())
+        .execute(self.pool).await?;
 
-    sqlx::query(&build_query(lock.is_shared(), true))
-        .bind(lock.key())
-        .execute(pool).await?;
+        Ok(())
+    }
 
-    Ok(())
-}
+    pub async fn try_lock(&self) -> Result<bool, sqlx::Error> {
+        let ret: (bool, ) = sqlx::query_as(&build_query(self.is_shared(), false))
+            .bind(self.key())
+            .fetch_one(self.pool).await?;
+    
+        Ok(ret.0)
+    }
 
-pub async fn try_lock(pool: &PgPool, lock: &DistributedLock) -> Result<bool, sqlx::Error> {
-    let ret: (bool, ) = sqlx::query_as(&build_query(lock.is_shared(), false))
-        .bind(lock.key())
-        .fetch_one(pool).await?;
-
-    Ok(ret.0)
-}
-
-pub async fn unlock(pool: &PgPool, lock: &DistributedLock) -> Result<(), sqlx::Error> {
-    sqlx::query("SELECT pg_catalog.pg_advisory_unlock($1)")
-        .bind(lock.key())
-        .execute(pool).await?;
-
-    Ok(())
+    pub async fn unlock(&self) -> Result<(), sqlx::Error> {
+        sqlx::query("SELECT pg_catalog.pg_advisory_unlock($1)")
+            .bind(self.key())
+            .execute(self.pool).await?;
+    
+        Ok(())
+    }
 }
 
 fn build_query(is_shared: bool, is_wait: bool) -> String {
